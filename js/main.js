6ae60f519d689efce9ae8d680804291251020304
Iryna_Samoilenko1 <Iryna_Samoilenko1@epam.com>
2020-01-24 18:51:11 +0200

    done

--- js/main.js
+++ js/main.js
@@ -1,25 +1,30 @@
 document.getElementById('output').style.visibility = 'hidden';
+let i = 0;
 
-function createP() {
-    return document.createElement("p");
-}
-
-document.getElementById('kmInput').addEventListener('input',  function(e) {
+document.getElementById('btn convert').addEventListener('click',  function(e) {
     document.getElementById('output').style.visibility = 'visible';
-    let km = e.target.value;
+    let km = document.getElementById('kmInput').value;
     document.getElementById('mOutput').innerHTML = km * 1000;
     document.getElementById('smOutput').innerHTML = km * 100000;
     document.getElementById('mlOutput').innerHTML = km * 1000000;
 });
 
 function newReview() {
-  let review = document.createElement("div");
-  let inputValue = document.getElementById("review").value;
-  let input = document.createTextNode(inputValue);
-  review.appendChild(input);
-  if (inputValue === '') {
+  let div = document.createElement("div");
+  let reviewValue = document.getElementById("review").value;
+  if (reviewValue === '') {
           alert("Write something, please");
       } else {
-          document.getElementById("reviews").appendChild(review);
+          div.className = `review`;
+          div.id = `${i}`;
+          let input = document.createTextNode(`ID#${i}: ` + reviewValue);
+          div.appendChild(input);
+          div.style.border = "thin dotted DimGray";
+          div.style.color = "ForestGreen";
+          div.style.margin = "10px";
+          div.style.fontSize = "0.8em"
+          div.style.display = "block"
+          document.getElementById("reviews").appendChild(div);
+          i++;
       }
 }
